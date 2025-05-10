2f53e478e35e0acbf89823a8e4e9298a43626629
louise.wiljander <louisewiljander@live.com>
2025-05-10 19:39:52 +0200

    Small updates

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from extensions import db, migrate  # Import db and migrate from extensions.py
 from create_app import create_app
